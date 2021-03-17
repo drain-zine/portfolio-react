1ac56e948570e365cfc26879dac38d907f4b4bd6
Based InvisibleTree <invisibletree@protonmail.ch>
2021-03-17 16:08:22 +0000

    init commit + broken anims :(

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
-import logo from './logo.svg';
+import React from "react";
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import { TransitionGroup, CSSTransition } from "react-transition-group";
+
 import './App.scss';
+import './animations/slideTransition.scss';
 
 import Lander from './components/Lander';
+import Design from './components/Design';
+
 
 function App() {
   return (
-    <div className="App">
-      <Lander />
-    </div>
+    <Router>
+      <div className="App">
+        <Route render={({ location }) => {
+          const currentKey = location.pathname.split("/")[1] || "/";
+          console.log(currentKey);
+          const timeout = {enter: 800, exit: 400};
+
+            return (
+              <TransitionGroup component={null}>
+                <CSSTransition key={currentKey} timeout={timeout} classNames="pageSlider" mountOnEnter={true} unmountOnExit={true}>
+                  <Switch location={location}>
+                    <Route path="/" exact component={Lander} />
+                    <Route path="/design" exact component={Design} />
+                  </Switch>
+                </CSSTransition>
+              </TransitionGroup>
+            )
+        }}/>
+      </div>
+    </Router>
   );
 }
 
--- src/components/Design.js
+++ src/components/Design.js
@@ -2,9 +2,20 @@ import React from 'react';
 
 const Design = () => {
     return(
-        <main className="lander relative ">
-            <div class="ml-24 mt-24 absolute z-20">
+        <main className="lander flex-grid mt-24">
+            <div className="col inline-block ml-24 z-20 w-1/2 mr-0">
                 <h1>Design</h1>
+            </div>
+
+            <div className="w-1/2 col">
+                <div className="flex-grid">
+                    <div className="col">
+                    
+
+                    </div>
+                    <div className="col"></div>
+                    <div className="col"></div>
+                </div>
             </div>
         </main>
     );
