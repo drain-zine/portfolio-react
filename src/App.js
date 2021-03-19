b11c28a5ea03ec6674e1715d69544f0b23f17673
Based InvisibleTree <invisibletree@protonmail.ch>
2021-03-19 18:45:28 +0000

    MenuImplementatioN!

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import { TransitionGroup, CSSTransition } from "react-transition-group";
+import {Helmet} from "react-helmet";
 
 import './App.scss';
 import './animations/slideTransition.scss';
@@ -8,11 +9,21 @@ import './animations/slideTransition.scss';
 import Lander from './components/Lander';
 import Design from './components/Design';
 
-
 function App() {
   return (
     <Router>
       <div className="App">
+        <Helmet>
+          <meta name="author" content="Tom Stannett" />
+          <meta name="description" content="Portal." />
+          <meta charSet="UTF-8" />
+
+          <meta property="og:title" content="TOM STANNETT" />
+          <meta property="og:url" content="https://www.drainzine.fr" />
+          <meta property="og:image" content="https://ia.media-imdb.com/images/rock.jpg" />
+          
+        </Helmet>
+
         <Route render={({ location }) => {
           const currentKey = location.pathname.split("/")[1] || "/";
           console.log(currentKey);
--- src/components/Design.js
+++ src/components/Design.js
@@ -1,22 +1,30 @@
 import { useEffect, useState } from 'react';
 import DesignCard from './DesignCard';
 
+import $ from 'jquery'; 
 import {designs_raw} from './../data/Designs.js';
 
 const Design = () => {
+    // state decs
     const [designs] = useState(designs_raw);
-
     const [docHeight, setDocHeight] = useState([]);
-
     const [lineRandYOffset, setLineRandYOffset] = useState([]);
+    
 
      // animate lines
      useEffect(() => {
+        // reset page to top
+        window.scrollTo(0, 0);
+
+        // vars for menu fades
+        const delayTime = 200;
+        const fadeTime = 300;
+
         // set doc height state to scale line container
         var cardContainerParent = document.getElementsByClassName("cardContainer")[0].parentElement;
         var cardContainerParentHeight = cardContainerParent.offsetHeight;
         var sbHeight = window.innerHeight * (window.innerHeight / document.body.offsetHeight);
-        
+        var startingLength = 3;
 
         cardContainerParentHeight += sbHeight;
 
@@ -46,14 +54,29 @@ const Design = () => {
         process.nextTick(() => {
             for(let i = 0; i < paths.length; i++){
                 let pathLength = paths[i].getTotalLength();
-               // adjust for random positioning increment
-                
+               
+                // adjuts dash and offset such that we hide the line
                 paths[i].style.strokeDasharray = pathLength + ' ' + pathLength;
-                paths[i].style.strokeDashoffset = pathLength; 
+                paths[i].style.strokeDashoffset = pathLength - startingLength; 
                 
                 pathLengths[i] = pathLength;
             }
         });
+
+        $.fn.reverse = [].reverse;
+        $(".menuBtn").mouseenter(function(){
+            $(".menu div").reverse().each(function(i) {   
+                $(this).delay(delayTime * i).fadeTo(fadeTime,1);
+            });
+        });
+
+        $(".dropdown").mouseleave(function(){
+            console.log("Hello");
+            $(".menu div").each(function(i) {   
+                $(this).delay(delayTime * i).fadeTo(fadeTime,0);
+            });
+        });
+
 
         // scroll event handler
         window.onscroll = () => {
@@ -86,17 +109,26 @@ const Design = () => {
         <main className="lander flex mt-24">
             <div className="flex-initial inline-block ml-24 z-20 mr-0">
                 <h1>Design</h1>
-
-                <div className="dropDownContainer fixed"></div>
             </div>
 
             <div className="flex-initial relative">
-                
                     {designs.map((design,index) => (
                         <DesignCard design={design} docHeight={docHeight[index]} lineRandYOffset={lineRandYOffset[index]}/>
                         
                     ))}
                 
+            </div>
+
+            <div className="dropdown fixed flex flex-col m-8 p-3 ">
+                <div className="menu">
+                    <div><a href="/index.html">Home</a></div>
+                    <div><a href="/portfolio.html">Portfolio</a></div>	
+                    <div><a href="/contact.html">Contact</a></div>	
+                </div>
+
+                <div className="menuBtn cursor-pointer">
+                    <h3>Menu</h3>
+                </div>
             </div>
         </main>
     );
