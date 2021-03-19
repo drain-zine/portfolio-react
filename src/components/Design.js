14f0e5db6112e513dba4cf9a1095998324bd47e3
Based InvisibleTree <invisibletree@protonmail.ch>
2021-03-19 11:34:05 +0000

    Init anim commit

--- src/components/Design.js
+++ src/components/Design.js
@@ -7,40 +7,17 @@ const Design = () => {
     const [designs] = useState(designs_raw);
 
     return(
-        <main className="lander flex-grid mt-24">
-            <div className="col inline-block ml-24 z-20 w-1/2 mr-0">
+        <main className="lander flex mt-24">
+            <div className="flex-initial inline-block ml-24 z-20 mr-0">
                 <h1>Design</h1>
             </div>
 
-            <div className="w-1/2 col">
-                {/* map each new design card to a position in the landing page table */}
-                {designs.map((design) => {
-                    if(design.x_pos === 1){
-                        return(
-                            <div className="flex-grid cardContainer">
-                                <div className="col"><DesignCard design={design}/></div>
-                                <div className="col"></div>
-                                <div className="col"></div>
-                            </div>
-                        );
-                    }else if(design.x_pos === 2){
-                        return(
-                            <div className="flex-grid cardContainer">
-                                <div className="col"></div>
-                                <div className="col"><DesignCard design={design}/></div>
-                                <div className="col"></div>
-                            </div>
-                        );
-                    }else{
-                        return(
-                            <div className="flex-grid cardContainer">
-                                <div className="col"></div>
-                                <div className="col"></div>
-                                <div className="col"><DesignCard design={design}/></div>
-                            </div>
-                        );
-                    }
-                })}
+            <div className="flex-initial relative">
+                
+                    {designs.map((design) => (
+                        <DesignCard design={design}/>
+                    ))}
+                
             </div>
         </main>
     );
--- src/components/DesignCard.js
+++ src/components/DesignCard.js
@@ -1,26 +1,68 @@
+import React, { useEffect, useState} from 'react';
+
 const DesignCard = ({design}) => {
+    const [scrollPercent, setScrollPercent] = useState(0);
 
-    return(
-        <div class={"mt-" + design.spacing_top.toString() + " ml-"+ design.spacing_left.toString()}>
+    var pos = Math.floor(Math.random() * 5).toString();
 
-            {/* addressing format nm -> n = row, m = col */}
-            {/* image row - we use require on img source for same behaviour as importing it */}
-            <div className="flex-grid">
-                <div className="col"><p>{design.text_1x1}</p></div> 
-                <div className="col"><img src={design.img}/></div>
-                <div className="col"><p>{design.text_1x3}</p></div>
-            </div>
+    
 
-            {/* lower content panel */}
-            <div className="flex-grid">
-                <div className="col"><p>{design.text_2x1}</p></div>
-                <div className="col"><p>{design.text_2x2}</p></div>
-                <div className="col"><p>{design.text_2x3}</p></div>
-            </div>
+    
+
+    useEffect(() => {
+        const path = document.getElementById('line-path');
+        var pathLength = path.getTotalLength();
+
+        path.style.strokeDasharray = pathLength + ' ' + pathLength;
+        path.style.strokeDashoffset = pathLength; 
+
+        var drawLength = 0;
+        console.log("Path Length: " + pathLength);
+    
+        window.onscroll = () => {
+            setScrollPercent((document.documentElement.scrollTop + document.body.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight));
+            drawLength = pathLength * ((document.documentElement.scrollTop + document.body.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight));
+            path.style.strokeDashoffset = pathLength - drawLength;
+            console.log("DrawLength: " + drawLength.toString() + "offset: " + (pathLength - drawLength).toString() );
+        }     
+      }, []);
+
+    console.log(scrollPercent);
+
+    if(design.type === 1){
+        return(
+           <div className={"cardContainer flex mt-" + design.spacing_top.toString() + " mb-" + design.spacing_bottom.toString() +" mlr-"+pos}>
+
+            <svg className="absolute" id="line-svg">
+                <path id="line-path" className="animate" fill="none" stroke="white" stroke-width="1" d="M1 0 L1 100"/>
+            </svg>
+            <div className="imgContainer flex-1"><img src={design.img}/></div>
+            <div className="textContainer flex-1"><p>{design.text_1}</p></div>
         </div>
 
-    );
+        );
+    }else if(design.type == 2){
+        return(
+            <div className={"cardContainer flex mt-" + design.spacing_top.toString() + " mb-" + design.spacing_bottom.toString() +" mlr-"+pos}>
+
+                <div className="textContainer flex-1"><p>{design.text_1}</p></div>
+                <div className="imgContainer flex-1"><img src={design.img}/></div>
+            </div>
+        );
+
+    }else if(design.type === 3){
+        return (
+            <div className={"cardContainer flex mt-" + design.spacing_top.toString() + " mb-" + design.spacing_bottom.toString() +" mlr-"+pos}>
+                <div className="imgContainer flex-1"><img src={design.img}/></div>
+                <div className="paragraphContainer flex-1 flex">
+                    <div id="col_1" className="textContainer flex-1 ml-1"><p>{design.text_1}</p></div>
+                    <div id="col_2" className="textContainer flex-1 ml-1"><p>{design.text_2}</p></div>
+                </div>
+            </div>
+        );
+    } 
 }
+
 
 
 export default DesignCard;
