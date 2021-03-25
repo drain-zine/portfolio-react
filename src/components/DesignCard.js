a601741fffd93c94b1c91a16dc695e845913e91f
Based InvisibleTree <invisibletree@protonmail.ch>
2021-03-25 11:44:13 +0000

    Animation handling cleanup

--- src/components/DesignCard.js
+++ src/components/DesignCard.js
@@ -1,15 +1,54 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Link } from 'react-router-dom';
-
+import $ from 'jquery'; 
 import CardImg from './CardImg';
 
 const DesignCard = (props) => {
-    const {design,docHeight,lineRandYOffset} = props;
+    const {design,fontColor} = props;
     
+    const ref = useRef(null);
+
+    const [docHeight, setDocHeight] = useState(0);
+    const [lineRandYOffset, setLineRandYOffset] = useState(0);
+
     var pos = design.rand_x ? Math.floor(Math.random() * 5).toString() : design.x_pos;
     const ConditionalLink = ({ children, to, condition }) => (!!condition && to)
       ? <Link to={to}>{children}</Link>
       : <>{children}</>;
+
+      // init lines for animation in container
+      useEffect(() => {
+        if(ref.current){
+            var startingLength = 3;
+
+            var card = ref.current.parentElement.parentElement;
+            
+        
+            // vars for scroll anim
+            const path = ref.current.firstElementChild;
+            var pathLength = 0;
+
+            let rand_offset = 3+Math.floor(Math.random() * 4);
+            setLineRandYOffset(rand_offset);
+            
+
+            setTimeout(function(){
+                var cardContainerParentHeight=$("main").outerHeight(true);
+                var card_y = card.getBoundingClientRect().top;
+                console.log("test " + cardContainerParentHeight);
+                pathLength = (cardContainerParentHeight - card_y)*(1+(rand_offset/100));
+                setDocHeight(pathLength);
+                console.log("p: " + pathLength );
+                
+                // adjuts dash and offset such that we hide the line
+                path.style.strokeDasharray = pathLength + ' ' + pathLength;
+                path.style.strokeDashoffset = pathLength - startingLength; 
+                path.style.stroke = fontColor;
+            },300);
+        }else{
+            console.log("Waiting on design card load");
+        }
+      }, [ref.current]);
 
     if(design.type === 1){
         return(
@@ -17,8 +56,8 @@ const DesignCard = (props) => {
 
             
             <div style={{width: design.img_size+"%"}} className="imgContainer flex-1 relative">
-                <svg viewBox={"0 0 100 " + docHeight} className="absolute z-0" id="line-svg" height={docHeight} style={{transform: "translateY(-"+lineRandYOffset+"%)"}}>
-                    <line x1="50%" y1="0%" x2="50%" y2="100%"  className="line-path" fill="none" stroke="white" strokeWidth="1" />
+                <svg ref={ref} viewBox={"0 0 100 " + docHeight} className="absolute z-0" id="line-svg" height={docHeight} style={{transform: "translateY(-"+lineRandYOffset+"%)"}}>
+                    <line  x1="50%" y1="0%" x2="50%" y2="100%"  className="line-path" fill="none" stroke="white" strokeWidth="1" />
                 </svg>
                 <ConditionalLink to={design.link_url} condition={design.link}>
                     <CardImg src={design.img} size={design.img_size}/>      
@@ -34,8 +73,8 @@ const DesignCard = (props) => {
 
                 <div className="textContainer flex-1"><p>{design.text_1}</p></div>
                 <div style={{width: design.img_size+"%"}} className="imgContainer relative">
-                    <svg viewBox={"0 0 100 " + docHeight} className="absolute z-0" id="line-svg" height={docHeight} style={{transform: "translateY(-"+lineRandYOffset+"%)"}}>
-                        <line x1="50%" y1="0%" x2="50%" y2="100%"  className="line-path" fill="none" stroke="white" strokeWidth="1" />
+                    <svg ref={ref} viewBox={"0 0 100 " + docHeight} className="absolute z-0" id="line-svg" height={docHeight} style={{transform: "translateY(-"+lineRandYOffset+"%)"}}>
+                        <line  x1="50%" y1="0%" x2="50%" y2="100%"  className="line-path" fill="none" stroke="white" strokeWidth="1" />
                     </svg>
                     <CardImg src={design.img} size={design.img_size}/>
                 </div>
@@ -46,8 +85,8 @@ const DesignCard = (props) => {
         return (
             <div style={{width: design.card_width+"%"}} className={"cardContainer flex mt-" + design.spacing_top.toString() + " pb-" + design.spacing_bottom.toString() +" mlr-"+pos}>
                 <div className="imgContainer relative flex-2">
-                <svg viewBox={"0 0 100 " + docHeight} className="absolute z-0" id="line-svg" height={docHeight} style={{transform: "translateY(-"+lineRandYOffset+"%)"}}>
-                        <line x1="50%" y1="0%" x2="50%" y2="100%"  className="line-path" fill="none" stroke="white" strokeWidth="1" />
+                <svg ref={ref} viewBox={"0 0 100 " + docHeight} className="absolute z-0" id="line-svg" height={docHeight} style={{transform: "translateY(-"+lineRandYOffset+"%)"}}>
+                        <line  x1="50%" y1="0%" x2="50%" y2="100%"  className="line-path" fill="none" stroke="white" strokeWidth="1" />
                     </svg>
                     <ConditionalLink to={design.link_url} condition={design.link}>
                         <CardImg src={design.img} size={design.img_size}/>      
@@ -64,7 +103,7 @@ const DesignCard = (props) => {
             <div style={{width: design.card_width+"%"}} className={"cardContainer flex mt-" + design.spacing_top.toString() + " pb-" + design.spacing_bottom.toString() +" mlr-"+pos}>
 
                 <div style={{width: design.img_size+"%"}} className="imgContainer flex-1 relative">
-                    <svg viewBox={"0 0 100 " + docHeight} className="absolute z-0" id="line-svg" height={docHeight} style={{transform: "translateY(-"+lineRandYOffset+"%)"}}>
+                    <svg ref={ref} viewBox={"0 0 100 " + docHeight} className="absolute z-0" id="line-svg" height={docHeight} style={{transform: "translateY(-"+lineRandYOffset+"%)"}}>
                         <line x1="50%" y1="0%" x2="50%" y2="100%"  className="line-path" fill="none" stroke="white" strokeWidth="1" />
                     </svg>
                     <CardImg src={design.img} size={design.img_size}/>
@@ -75,8 +114,8 @@ const DesignCard = (props) => {
         return (
             <div style={{width: design.card_width+"%"}} className={"cardContainer flex mt-" + design.spacing_top.toString() + " pb-" + design.spacing_bottom.toString() +" mlr-"+pos}>
                 <div style={{width: design.img_size+"%"}} className="imgContainer relative flex-1">
-                    <svg viewBox={"0 0 100 " + docHeight} className="absolute z-0" id="line-svg" height={docHeight} style={{transform: "translateY(-"+lineRandYOffset+"%)"}}>
-                        <line x1="50%" y1="0%" x2="50%" y2="100%"  className="line-path" fill="none" stroke="white" strokeWidth="1" />
+                    <svg ref={ref} viewBox={"0 0 100 " + docHeight} className="absolute z-0" id="line-svg" height={docHeight} style={{transform: "translateY(-"+lineRandYOffset+"%)"}}>
+                        <line  x1="50%" y1="0%" x2="50%" y2="100%"  className="line-path" fill="none" stroke="white" strokeWidth="1" />
                     </svg>
                     <CardImg src={design.img} size={design.img_size}/>
                     
@@ -95,8 +134,8 @@ const DesignCard = (props) => {
                     <div id="row_2" className="textContainer flex-initial mt-3 ml-1"><p>{design.text_2}</p></div>
                 </div>
                 <div style={{width: design.img_size+"%"}} className="imgContainer flex-inital relative">
-                    <svg viewBox={"0 0 100 " + docHeight} className="absolute z-0" id="line-svg" height={docHeight} style={{transform: "translateY(-"+lineRandYOffset+"%)"}}>
-                        <line x1="50%" y1="0%" x2="50%" y2="100%"  className="line-path" fill="none" stroke="white" strokeWidth="1" />
+                    <svg ref={ref} viewBox={"0 0 100 " + docHeight} className="absolute z-0" id="line-svg" height={docHeight} style={{transform: "translateY(-"+lineRandYOffset+"%)"}}>
+                        <line  x1="50%" y1="0%" x2="50%" y2="100%"  className="line-path" fill="none" stroke="white" strokeWidth="1" />
                     </svg>
                     <CardImg src={design.img} size={design.img_size}/>
                     
--- src/components/DesignContainer.js
+++ src/components/DesignContainer.js
@@ -1,87 +1,39 @@
-import { useEffect, useState } from 'react';
-import $ from 'jquery'; 
+import { useEffect } from 'react';
 
 import DesignCard from '../components/DesignCard';
-import Menu from '../components/Menu.js';
 
 const DesignContainer = (props) => {  
     const {data,title,color, fontColor} = props;
-    const [docHeight, setDocHeight] = useState([]);
-    const [lineRandYOffset, setLineRandYOffset] = useState([]);
-    
-
+        
      // animate lines
      useEffect(() => {
-         /* INIT */
-        // reset page to top
-        //window.scrollTo(0, 0);
-
-        // set doc height state to scale line container
-        var cardContainerParent = document.getElementsByClassName("cardContainer")[0].parentElement;
-        var cardContainerParentHeight = cardContainerParent.offsetHeight;
-       
-        var startingLength = 3;
-
-        /* cardContainerParentHeight += sbHeight; */
-      
-        
-        var cards = document.getElementsByClassName("cardContainer");
-    
-        // vars for scroll anim
-        const paths = document.getElementsByClassName('line-path');
-        var pathLengths = [];
-        var drawLength = 0;
+        const paths = document.getElementsByClassName("line-path");
 
         // vars for upward scrolling behaviour
         var lastScrollTop = 0;
         var scrollUp = false;
 
         // pre specify this -> allows us to address values
-        var lastDrawLengths = new Array(cards.length);
-
-        /* setTimeout(function(){console.log("wawgwan: " + $("main").outerHeight(true));},200); */
-
-        // wait until next tick -> if we do this immediately, dom renderer will have yet to scale path to doc height state and thus our line will have path length 0
-        /* process.nextTick(() => { */
-        setTimeout(function(){
-            cardContainerParentHeight=$("main").outerHeight(true);
-            console.log("test timeout :): " + cardContainerParentHeight);
-           
-            for(let i = 0; i < paths.length; i++){
-                let rand_offset = 3+Math.floor(Math.random() * 4);
-                let card_y = cards[i].getBoundingClientRect().top;
-                setLineRandYOffset(lineRandYOffset => [...lineRandYOffset, rand_offset]);
-                
-                setDocHeight(docHeight => [...docHeight, (cardContainerParentHeight - card_y)*(1+(rand_offset/100))]);
-
-                let pathLength = (cardContainerParentHeight - card_y)*(1+(rand_offset/100));
-                
-                
-                // adjuts dash and offset such that we hide the line
-                paths[i].style.strokeDasharray = pathLength + ' ' + pathLength;
-                paths[i].style.strokeDashoffset = pathLength - startingLength; 
-                paths[i].style.stroke = fontColor;
-                pathLengths[i] = pathLength;
-            }
-        }, 300);
-
-
+        var lastDrawLengths = new Array(paths.length);
 
         // scroll event handler
         window.onscroll = () => {
+            var drawLength = 0;
             var st = window.pageYOffset || document.documentElement.scrollTop;
             // check if scrolling down
             if(st > lastScrollTop){
                 // handle drawing of each path
+                
                 for(let i = 0; i < paths.length; i++){
+                    let pathLength = paths[i].getTotalLength();
                     // only apply if scrolling down, and if beyond current line
                     if(!scrollUp || drawLength >= lastDrawLengths[i]){
-                        drawLength = pathLengths[i] * ((document.documentElement.scrollTop + document.body.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight));
-                        console.log("p: " + pathLengths[i] + " d: " + drawLength);
-                        paths[i].style.strokeDashoffset = pathLengths[i] - drawLength;
+                        drawLength = pathLength * ((document.documentElement.scrollTop + document.body.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight));
+                        console.log("p: " + pathLength + " d: " + drawLength);
+                        paths[i].style.strokeDashoffset = pathLength - drawLength;
                     
                         // update last draw lengths
-                        lastDrawLengths.push(drawLength);
+                        lastDrawLengths[i] = drawLength;
                     }
                 }
             }else{
@@ -100,8 +52,8 @@ const DesignContainer = (props) => {
             </div>
 
             <div className="flex-initial relative test">
-                {data.map((design,index) => (
-                    <DesignCard design={design} docHeight={docHeight[index]} lineRandYOffset={lineRandYOffset[index]}/>
+                {data.map((design) => (
+                    <DesignCard design={design} fontColor={fontColor}/>
                     
                 ))}
             </div>       
