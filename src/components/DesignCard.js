e9c6f8f74cc8c3c012fa3c4e18f6e73315826107
Tom Stannett <invisibletree@protonmail.ch>
2021-04-08 12:22:12 +0100

    init commit for AV

--- src/components/DesignCard.js
+++ src/components/DesignCard.js
@@ -12,10 +12,10 @@ const DesignCard = (props) => {
 
     if(design.type === 1){
         return(
-           <div style={{width: design.card_width+"%"}} className={(design.link? 'cardLink' : '') + " cardContainer flex lg:mt-" + design.spacing_top.toString() + " lg:pb-" + design.spacing_bottom.toString() +" lg:"+(pos < 0 ? "-" : "")+ "ml-"+ Math.abs(pos) + "p mmd:flex-col mmd:justify-center mmd:w-full mmd:text-2xl mmd:mb-48"}>
+           <div style={{width: design.card_width+"%"}} className={(design.link? 'cardLink' : '') + " cardContainer flex lg:mt-" + design.spacing_top.toString() + " lg:pb-" + design.spacing_bottom.toString() +" lg:"+(pos < 0 ? "-" : "")+ "ml-"+ Math.abs(pos) + "p mmd:flex-col mmd:justify-center mmd:w-full mmd:text-2xl mmd:mb-48 mmd:items-center"}>
 
             
-            <div style={{width: design.img_size+"%"}} className="imgContainer flex-1 relative">
+            <div style={{width: design.img_size+"%"}} className="imgContainer flex-1 relative mmd:w-full mmd:mb-4">
                 <ConditionalLink to={design.link_url} condition={design.link}>
                     <CardImg src={design.img} size={design.img_size}/>      
                 </ConditionalLink>
@@ -53,7 +53,7 @@ const DesignCard = (props) => {
         );
     }else if(design.type === 4){
         return (
-            <div style={{width: design.card_width+"%"}} className={"cardContainer flex mt-" + design.spacing_top.toString() + " pb-" + design.spacing_bottom.toString() +" mlr-"+pos}>
+            <div style={{width: design.card_width+"%"}} className={"cardContainer flex lg:mt-" + design.spacing_top.toString() + " lg:pb-" + design.spacing_bottom.toString() +" lg:"+(pos < 0 ? "-" : "")+ "ml-"+ Math.abs(pos) + "p mmd:flex-row mmd:justify-center mmd:w-full  mmd:mb-48 mmd:relative mmd:px-4"}>
 
                 <div style={{width: design.img_size+"%"}} className="imgContainer flex-1 relative">
                     <CardImg src={design.img} size={design.img_size}/>
@@ -62,8 +62,8 @@ const DesignCard = (props) => {
         );
     }else if(design.type === 5){
         return (
-            <div style={{width: design.card_width+"%"}} className={"cardContainer flex mt-" + design.spacing_top.toString() + " pb-" + design.spacing_bottom.toString() +" mlr-"+pos}>
-                <div style={{width: design.img_size+"%"}} className="imgContainer relative flex-1">
+            <div style={{width: design.card_width+"%"}} className={" cardContainer flex lg:mt-" + design.spacing_top.toString() + " lg:pb-" + design.spacing_bottom.toString() +" lg:"+(pos < 0 ? "-" : "")+ "ml-"+ Math.abs(pos) + "p mmd:flex-col mmd:justify-center mmd:w-full mmd:text-2xl mmd:mb-48 mmd:items-center"}>
+                <div style={{width: design.img_size+"%"}} className="imgContainer relative flex-1 mmd:w-full mmd:mb-4">
                     <CardImg src={design.img} size={design.img_size}/>
                     
                 </div>
@@ -75,12 +75,12 @@ const DesignCard = (props) => {
         );
     }else if(design.type === 6){
         return (
-            <div style={{width: design.card_width+"%"}} className={"cardContainer flex mt-" + design.spacing_top.toString() + " pb-" + design.spacing_bottom.toString() +" mlr-"+pos}>
-                <div className="paragraphContainer flex-1 flex flex-col">
-                    <div id="row_1" className="textContainer flex-initial ml-1"><p>{design.text_1}</p></div>
-                    <div id="row_2" className="textContainer flex-initial mt-3 ml-1"><p>{design.text_2}</p></div>
+            <div style={{width: design.card_width+"%"}} className={" cardContainer flex lg:mt-" + design.spacing_top.toString() + " lg:pb-" + design.spacing_bottom.toString() +" lg:"+(pos < 0 ? "-" : "")+ "ml-"+ Math.abs(pos) + "p mmd:flex-col mmd:justify-center mmd:w-full mmd:text-2xl mmd:mb-48 mmd:items-center mmd:relative"}>
+                <div className="paragraphContainer lg:flex-1 flex mmd:flex-col mmd:absolute mmd:left-1/2 mmd:top-1/2 mmd:z-10 mmd:w-full mmd:transform mmd:-translate-x-1/2 mmd:-translate-y-1/2 mmd:px-4 mmd:z-30">
+                    <div id="row_1" className="textContainer flex-initial ml-1 mmd:w-full"><p>{design.text_1}</p></div>
+                    <div id="row_2" className="textContainer flex-initial mt-3 ml-1 mmd:w-full"><p>{design.text_2}</p></div>
                 </div>
-                <div style={{width: design.img_size+"%"}} className="imgContainer flex-inital relative">
+                <div style={{width: design.img_size+"%"}} className="imgContainer flex-inital relative mmd:w-full">
                     <CardImg src={design.img} size={design.img_size}/>
                     
                 </div>
@@ -88,7 +88,7 @@ const DesignCard = (props) => {
         );
     }else if(design.type === 7){
         return (
-            <div style={{width: design.card_width+"%"}} className={"cardContainer flex mt-" + design.spacing_top.toString() + " pb-" + design.spacing_bottom.toString() +" mlr-"+pos + " text-sm"}>
+            <div style={{width: design.card_width+"%"}} className={"cardContainer flex lg:mt-" + design.spacing_top.toString() + " lg:pb-" + design.spacing_bottom.toString() +" lg:"+(pos < 0 ? "-" : "")+ "ml-"+ Math.abs(pos) + "p text-sm mmd:text-left mmd:w-full"}>
                 <div className="paragraphContainer flex-1 flex">
                     <div id="col_1" className="textContainer flex-initial ml-1"><p>{design.text_1}</p></div>
                     <div id="col_2" className="textContainer flex-initial ml-1"><p>{design.text_2}</p></div>
