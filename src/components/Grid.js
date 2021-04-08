f94cd7af290331bfff9e40cc3c38baf775ddc049
Tom Stannett <invisibletree@protonmail.ch>
2021-04-08 13:30:26 +0100

    grid fix

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,19 +1,30 @@
 import React from "react";
+import { isMobile } from "react-device-detect";
 
 const Grid = () => {
-    return(
-        <div className="gridLines">
-            <div className="cell"></div>
-            <div className="cell"></div>
-            <div className="cell"></div>
-            <div className="cell"></div>
-            <div className="cell"></div>
-            <div className="cell"></div>
-            <div className="cell"></div>
-            <div className="cell"></div>
-            <div className="cell"></div>
-        </div>
-    );
+
+    if(isMobile){
+        return(
+            <div className="gridLines mmd:px-2">
+                <div className="cell"></div>
+                <div className="cell"></div>
+            </div>
+        );
+    }else{
+        return(
+            <div className="gridLines mmd:px-2">
+                <div className="cell"></div>
+                <div className="cell"></div>
+                <div className="cell"></div>
+                <div className="cell"></div>
+                <div className="cell"></div>
+                <div className="cell"></div>
+                <div className="cell"></div>
+                <div className="cell"></div>
+                <div className="cell"></div>
+            </div>
+        );
+    }
 };
 
 export default Grid;
